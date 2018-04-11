815fb2de99d4c2b0fb8a9a5ec12b253ee94fe1ba
soulcabbage <992756841@qq.com>
2018-04-11 15:18:46 +0800

    DataBase.cs changed

--- WebApplication1/App_Data/DataAccessLayer/DataBase.cs
+++ WebApplication1/App_Data/DataAccessLayer/DataBase.cs
@@ -259,7 +259,7 @@ namespace MyOnLineExam.DataAccessLayer
         public SqlDataReader RunProcGetReader(string ProcName, SqlParameter[] Params) 
         {
           SqlCommand Cmd = CreateCommand(ProcName, Params);
-          return Cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            return Cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
         }
 
         //公有方法，调用存储过程(带参数)
@@ -341,6 +341,59 @@ namespace MyOnLineExam.DataAccessLayer
             return Count;
 
         }
-	
-	}
+        //DataReader转Dataset
+        public static DataSet ConvertDataReaderToDataSet(SqlDataReader reader)
+        {
+            DataSet dataSet = new DataSet();
+            do
+            {
+                // Create new data table 
+
+                DataTable schemaTable = reader.GetSchemaTable();
+                DataTable dataTable = new DataTable();
+
+                if (schemaTable != null)
+                {
+                    // A query returning records was executed 
+
+                    for (int i = 0; i < schemaTable.Rows.Count; i++)
+                    {
+                        DataRow dataRow = schemaTable.Rows[i];
+                        // Create a column name that is unique in the data table 
+                        string columnName = (string)dataRow["ColumnName"]; //+ " // Add the column definition to the data table 
+                        DataColumn column = new DataColumn(columnName, (Type)dataRow["DataType"]);
+                        dataTable.Columns.Add(column);
+                    }
+
+                    dataSet.Tables.Add(dataTable);
+
+                    // Fill the data table we just created 
+
+                    while (reader.Read())
+                    {
+                        DataRow dataRow = dataTable.NewRow();
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            dataRow[i] = reader.GetValue(i);
+
+                        dataTable.Rows.Add(dataRow);
+                    }
+                }
+                else
+                {
+                    // No records were returned 
+
+                    DataColumn column = new DataColumn("RowsAffected");
+                    dataTable.Columns.Add(column);
+                    dataSet.Tables.Add(dataTable);
+                    DataRow dataRow = dataTable.NewRow();
+                    dataRow[0] = reader.RecordsAffected;
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+            while (reader.NextResult());
+            return dataSet;
+        }
+
+    }
 }
